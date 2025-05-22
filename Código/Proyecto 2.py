ea21df3306212b56fa93cdc49f77ea2a925363f3
Rodney-1520 <noneysolis@gmail.com>
2025-05-21 23:18:07 -0600

    Menu Principal  v2
    En esta versión del menú ya esta mas estético y agradable a la vista

--- Código/Proyecto 2.py
+++ Código/Proyecto 2.py
@@ -13,9 +13,12 @@ def salir():
 #Creacion de la ventana principal
 ventana = tk.Tk()
 ventana.title("Tetris")
-ventana.geometry("650x650")
+ventana.geometry("650x575")
 
-tk.Label(ventana, text="Menu Principal", font=("Arial", 16 , "bold")).pack(pady=10)
+#Personalizacion de la ventana
+ventana.configure(bg="darkblue")
+
+tk.Label(ventana, text="Menu Principal", font=("Arial", 16 , "bold"),bg="darkblue",fg="orange").pack(pady=10)
 
 opciones = ["1.Continuar juego","2.Nuevo juego","3.Estadisticas de juegos","4.Salir"]
 
@@ -23,13 +26,13 @@ i = 1
 
 for texto in opciones:
      if  i == 1:
-          boton = tk.Button(ventana, text=texto, width=50, height=3, command=lambda i=i: coomingSoon(i))
+          boton = tk.Button(ventana, text=texto, font=("Arial", 12 , "bold"),bg="green",fg="white", width=40, height=2, command=lambda i=i: coomingSoon(i))
      elif  i == 2:
-          boton = tk.Button(ventana, text=texto, width=50, height=3, command=lambda i=i: coomingSoon(i))
+          boton = tk.Button(ventana, text=texto, font=("Arial", 12 , "bold" ),bg="red",fg="white", width=40, height=2, command=lambda i=i: coomingSoon(i))
      elif  i == 3:
-          boton = tk.Button(ventana, text=texto, width=50, height=3, command=lambda i=i: coomingSoon(i))
+          boton = tk.Button(ventana, text=texto, font=("Arial", 12 ,"bold" ),bg="gold",fg="white",  width=40, height=2, command=lambda i=i: coomingSoon(i))
      elif  i == 4:
-          boton = tk.Button(ventana, text=texto, width=50, height=3, command=salir)
+          boton = tk.Button(ventana, text=texto, font=("Arial", 12 ,"bold" ),bg="violet",fg="white", width=40, height=2, command=salir)
 
      boton.pack(pady=3)
      i += 1
